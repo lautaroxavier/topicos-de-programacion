a7342b4dcec45c2d513655db75db155bf79eed45
Lautaro Rodríguez Xavier <61719466+lautaroxavier@users.noreply.github.com>
2023-04-10 20:45:41 -0300

    reorganizacion de proyectos

---
+++ repaso/repaso/funciones.h
@@ -0,0 +1,14 @@
+#ifndef FUNCIONES_H_INCLUDED
+#define FUNCIONES_H_INCLUDED
+
+unsigned long long int factorial(int);
+
+unsigned long long int combinatorio(unsigned long long int, unsigned long long int);
+
+double eToXAprox(int, double);
+
+double raizCuadradaBabilonico(double, double);
+
+double seno(double, double);
+
+#endif // FUNCIONES_H_INCLUDED
---
+++ repaso/repaso/main.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include "funciones.h"
+#define TAM 6
+
+int main()
+{
+    return 0;
+}
